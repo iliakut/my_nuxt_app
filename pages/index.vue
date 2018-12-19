4dcb5d6945d3255146049ef762ac3c502ad05eed
Ilia <ilyakut@gmail.com>
2018-12-19 14:33:56 +0300

    changed index

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
-    _
+    <div>
+      This is my Nuxt App.
+    </div>
+    <div>
+      If you run fake server you can see users's list.
+      <nuxt-link to="/Users"
+                 class="nav-link" exact>
+        <a class="nav-link"
+           href="#">Список пользователей</a>
+      </nuxt-link>
+    </div>
+    <div>
+      If don't, you can try Add user page. It has First, Second Name validation, Email Validation, DropZone plugin to drag and drop your images to Imgur
+      <nuxt-link tag="li"
+                 to="/Add"
+                 class="nav-link"
+                 exact>
+        <a class="nav-link"
+           href="#">Добавить пользователя</a>
+      </nuxt-link>
+    </div>
   </div>
 </template>
 
